28ac8252f30296c6417ca83073efe4c7925150a5
Karen Calisaya <karenmcalisaya@gmail.com>
2022-04-20 17:15:09 -0300

    apuntes

--- src/middlewares/userLogs.js
+++ src/middlewares/userLogs.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
-const path = require('path');
 
 function logMiddleware(req, res, next){
-    let texto ="El usuario ingreso a la ruta: " + req.url + "\n"
+    let texto ="El usuario ingreso a la ruta: " + req.url + "\n"  /* le concatenamos la ruta- con req.url */
     fs.appendFileSync('src/logs/userLogs.txt', texto);
-    next();
+    next(); /* para pasar al controlador y ejecutar el metodo que corresponda */
 }
+/* appendFileSync, con esto agrega un regustro mas a ese archivo, no sobreescribe, mas bien lo agrega.
+Recibe dos parámetros: en donde deseo escribir, y otro que es lo que queremos escribir en el archivo.
+writeFileSync me sobreescribe todo el texto, borra lo anterior y guarda lo actual */
 
 module.exports = logMiddleware
+
+/* como es un middleware a nivel gloal hay que requerirlo en el app.js */
