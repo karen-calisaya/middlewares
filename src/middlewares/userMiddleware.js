14304214ffac69856804fb736bfb3f591a1cd197
Karen Calisaya <karenmcalisaya@gmail.com>
2022-04-20 17:30:24 -0300

    notas

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -1,12 +1,15 @@
 let userAdmin = ["Ada", "Greta", "Vim", "Tim"];
 
 const adminMiddleware = (req, res, next) =>{
-    if(userAdmin.includes(req.query.user)){
+    if(userAdmin.includes(req.query.user)){ /* si userAdmin (ARRAY) incluye el nombre que esta llegando por req.query.user que siga pasando */
         next();
 
     }else {
-        res.send("No tienes privilegios de entrar aca")
+        res.send("No tienes privilegios de entrar aca") /* si no no tiene permitido entrar aca */
     }
 }
 
 module.exports = adminMiddleware;
+
+/* este middleware debe gestionar si el nombre de usuario es el correcto
+para dejarte pasar.  */
--- src/routes/main.js
+++ src/routes/main.js
@@ -4,6 +4,6 @@ const controller = require('../controllers/indexController')
 const adminMiddleware = require('../middlewares/userMiddleware')
 
 router.get('/', controller.index);
-router.get('/admin', adminMiddleware, controller.admin)
-
+router.get('/admin', adminMiddleware, controller.admin) /* middleware para que ingresen los administradores */
+/* se va a verificar que el usuario sea un admin.. */
 module.exports = router;
